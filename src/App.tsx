9dd2a8a1f91346796d5c27e0839fdba0c292ec43
Idanref <idanref@gmail.com>
2025-08-16 23:29:54 +0300

    Revert "Test - 99% needs revert"
    This reverts commit aa5aa060ae20577e38f3b19452f43148d625f972.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import LandingPage from './components/LandingPage'
 import PrivacyPolicy from './components/PrivacyPolicy'
 import TermsConditions from './components/TermsConditions'
