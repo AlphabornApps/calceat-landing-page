17f3f087230d83204dbead3e9f70dbd357cfb426
Idanref <idanref@gmail.com>
2025-10-14 01:50:53 +0900

    Different icon for goals

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -86,7 +86,7 @@ const Testimonials: React.FC = () => {
           <div className="benefit-card animate-fade-in-up" style={{ animationDelay: '0.6s' }}>
             <div className="benefit-icon-large gradient-green">
               <svg width="36" height="36" viewBox="0 0 24 24" fill="currentColor">
-                <path d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z"/>
+                <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/>
               </svg>
             </div>
             <h3 className="benefit-card-title">Personalized Goals</h3>
